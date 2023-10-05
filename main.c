bd2c8d32ffb56a4dc780f5c21cd3e663dcadd1bf
Stepan Didurenko <sadidurenko@edu.hse.ru>
2023-10-06 00:14:32 +0300

    Optimized code (works 24 sec for n=32)

--- main.c
+++ main.c
@@ -1,35 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// Минимум из двух целочисленных значений
-int min(int a, int b)
-{
-  if (a < b)
-    return a;
-  return b;
-}
+int n;
+unsigned int array[32];
+unsigned long long all_sum;
 
 // Рекурсивная функция
-int getResult(int i, const int n, const int *array, int first_sum, int second_sum)
+unsigned long long getResult(register unsigned int i, register unsigned long long first_sum)
 {
   if (i == n)
-    return abs(first_sum - second_sum);
-  int variant1 = getResult(i + 1, n, array, first_sum + array[i], second_sum);
+  {
+    if (all_sum > 2 * first_sum)
+      return all_sum - 2 * first_sum;
+    return 2 * first_sum - all_sum;
+  }
+
+  register unsigned long long variant1 = getResult(i + 1, first_sum + array[i]);
   if (variant1 == 0)
     return 0;
-  int variant2 = getResult(i + 1, n, array, first_sum, second_sum + array[i]);
-  return min(variant1, variant2);
+  register unsigned long long variant2 = getResult(i + 1, first_sum);
+  if (variant1 <= variant2)
+    return variant1;
+  return variant2;
 }
 
 int main()
 {
-  int n;
   scanf("%d", &n);
 
-  int *array = malloc(sizeof(int) * n);
+  all_sum = 0;
   for (int i = 0; i < n; i++)
+  {
     scanf("%d", &array[i]);
+    all_sum += array[i];
+  }
 
-  int result = getResult(0, n, array, 0, 0);
-  printf("%d\n", result);
+  printf("%llu\n", getResult(0, 0));
 }
