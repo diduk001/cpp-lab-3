4d8047b2d492c90f7fc8794881e3d52f17e38753
Stepan Didurenko <sadidurenko@edu.hse.ru>
2023-10-07 01:37:19 +0300

    Added more optimizations

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int n;
+unsigned int n;
 unsigned int array[32];
 unsigned long long all_sum;
 
@@ -10,18 +10,26 @@ unsigned long long getResult(register unsigned int i, register unsigned long lon
 {
   if (i == n)
   {
-    if (all_sum > 2 * first_sum)
-      return all_sum - 2 * first_sum;
-    return 2 * first_sum - all_sum;
+    register unsigned long long double_first_sum = (first_sum << 1);
+    if (all_sum > double_first_sum)
+      return all_sum - double_first_sum;
+    else
+      return double_first_sum - all_sum;
   }
-
-  register unsigned long long variant1 = getResult(i + 1, first_sum + array[i]);
-  if (variant1 == 0)
-    return 0;
-  register unsigned long long variant2 = getResult(i + 1, first_sum);
-  if (variant1 <= variant2)
-    return variant1;
-  return variant2;
+  else
+  {
+    unsigned long long variant1 = getResult(i + 1, first_sum + array[i]);
+    if (variant1 == 0)
+      return 0;
+    else
+    {
+      unsigned long long variant2 = getResult(i + 1, first_sum);
+      if (variant1 <= variant2)
+        return variant1;
+      else
+        return variant2;
+    }
+  }
 }
 
 int main()
